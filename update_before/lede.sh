5e16804d6a9c0d3cf3940a4eb503c1de27a39ea4
zhr520v <63360959+zhr520v@users.noreply.github.com>
2024-12-31 18:16:52 +0800

    Update lede.sh

--- update_before/lede.sh
+++ update_before/lede.sh
@@ -4,8 +4,9 @@ rm -rf package/libs/mbedtls
 rm -rf package/libs/ustream-ssl
 rm -rf package/libs/uclient
 
-sudo curl -L -o rmoyulong.tar.gz https://github.com/rmoyulong/AX6-Actions_Lede/releases/download/union_package/lede.tar.gz
-tar zxvfp rmoyulong.tar.gz
-
 # 取消主题默认设置
 #find package/luci-theme-*/* -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+
+# Default IP
+sed -i 's/192.168.1.1/192.168.80.2/g' package/base-files/files/bin/config_generate
+
